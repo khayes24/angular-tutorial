3c1e9311f01df76172ea4cd934888cdf10994110
Kameron <khayes@hotohuseinc.com>
2018-08-21 21:25:43 -0400

    initial commit with course component added

---
+++ src/app/courses.component.ts
@@ -0,0 +1,10 @@
+//Import angular
+import { Component } from '@angular/core';
+//Declarator Function
+@Component({
+  selector: 'courses', //<courses> "course"  //<div class="courses"> ".course"
+  template: '<h2>Courses</h2>'
+})
+
+export class CoursesComponent {
+}
