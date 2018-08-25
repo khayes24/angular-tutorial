52ca683eefcdd06a39f80931181f5d45132fe95a
Kameron <khayes@hotohuseinc.com>
2018-08-25 18:12:16 -0400

    testing object binding/removing old test code

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -6,29 +6,22 @@ import { CoursesService } from './courses.service';
   selector: 'courses', //<courses> "course"  //<div class="courses"> ".course"
   //Using the backtick in the template attribute allows code to be broken up into multiple lines
   template: `
-  <input [(ngModel)]="email" (keyup.enter)="onKeyUp()"/>
-  <div (click)="onDivClicked()">
-    <button class="btn btn-primary" (click)="onSave($event)">Save</button>
-  </div>
+  {{course.title}}<br>
+  {{course.students | number}}<br>
+  {{course.rating | number:'1.2-2'}}<br>
+  {{course.price | currency:'AUD':true}}<br>
+  {{course.releaseDate | date:'shortDate'}}<br>
   `
 })
 
 export class CoursesComponent {
-  email = "me@example.com";
 
-  onKeyUp(){
-    //Angular handles key events already. this function is called on enter button being pressed
-      console.log(this.email);
-  }
-
-  onSave($event){//The $event gives access to event handler object
-    //Event bubbling goes up the dom tree. The inner event will show first then the outer div
-    //$event.stopPropogation(); will prevent event bubbling
-    console.log('YEAHHH', $event);
-  }
-
-  onDivClicked(){
-    console.log('Div clicked!');
+  course = {
+    title: "Complete Guy",
+    rating: 4.956,
+    students: 30123,
+    price: 190.95,
+    releaseDate: new Date(2018, 25, 8)
   }
 
   title = "List of courses";
