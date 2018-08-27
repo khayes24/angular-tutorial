621138e293c2ba6fa8ae9c1453f267066742e889
Kameron <khayes@hotohuseinc.com>
2018-08-27 19:21:29 -0400

    creating custom pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 //To create new component via cli -> ng g c componentName
 //To create new service via cli -> ng g s servicename
+//To create a custom pipe via cli -> ng g p pipeName
 
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { AngularFontAwesomeModule } from 'angular-font-awesome';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppComponent } from './app.component';
@@ -14,6 +14,8 @@ import { CoursesService } from './courses.service';
 import { AuthorsComponent } from './authors/authors.component';
 import { AuthorsService } from './authors.service';
 import { FavoriteComponent } from './favorite/favorite.component';
+import { CustompipeComponent } from './custompipe/custompipe.component';
+import { TitleCasePipe } from './title-case.pipe';
 
 @NgModule({
   declarations: [
@@ -21,12 +23,13 @@ import { FavoriteComponent } from './favorite/favorite.component';
     CourseComponent,
     CoursesComponent,
     AuthorsComponent,
-    FavoriteComponent
+    FavoriteComponent,
+    CustompipeComponent,
+    TitleCasePipe
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    AngularFontAwesomeModule,
     FontAwesomeModule
   ],
   //Registers all dependecies
---
+++ src/app/custompipe/custompipe.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-custompipe',
+  templateUrl: './custompipe.component.html',
+  styleUrls: ['./custompipe.component.css']
+})
+export class CustompipeComponent implements OnInit {
+
+  title: string;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
